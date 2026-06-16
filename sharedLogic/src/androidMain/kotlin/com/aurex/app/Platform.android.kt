dcd380a64437458755e146e6dcbb63b2228e8de6
SoftAyush <ayushtimalsina19@gmail.com>
2026-06-16 08:43:44 +0545

    Project Created

---
+++ sharedLogic/src/androidMain/kotlin/com/aurex/app/Platform.android.kt
@@ -0,0 +1,9 @@
+package com.aurex.app
+
+import android.os.Build
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
---
+++ sharedLogic/src/commonMain/kotlin/com/aurex/app/Platform.kt
@@ -0,0 +1,7 @@
+package com.aurex.app
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
